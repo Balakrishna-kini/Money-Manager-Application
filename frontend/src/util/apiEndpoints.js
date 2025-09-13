d0c056923d61bc57436fab3d96e5621cbc4ba4f7
Balakrishna-kini <balakrishnakini22@gmail.com>
2025-09-13 18:29:58 +0530

    Add/update apiEndPoints.js

--- frontend/src/util/apiEndpoints.js
+++ frontend/src/util/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_API_URL;  
+export const BASE_URL = "https://money-manager-application-backend.onrender.com/api/v1.0";
 // Example: "https://money-manager-application-backend.onrender.com/api/v1.0"
 
 const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
